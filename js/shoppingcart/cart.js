828e38bcf3ec5d6ef278fcefdab521ffa9e118aa
Tonix89 <Antonio.Arabejo@stud.noroff.no>
2022-10-08 23:51:49 +0200

    cart updated

--- js/shoppingcart/cart.js
+++ js/shoppingcart/cart.js
@@ -13,26 +13,42 @@ async function apiCall() {
 
     const details = result.values;
     cartBox.innerHTML = "";
-    const storage = sessionStorage.key(1);
-    if (storage) {
-      for (let i = 1; i < details.length; i++) {
-        const id = details[i][0];
-        console.log(id);
-        const myItems = sessionStorage.getItem(id);
+
+    const prdId = [];
+    for (let i = 1; i < details.length; i++) {
+      const id = details[i][0];
+      console.log(id);
+      prdId.push(id);
+    }
+    // console.log(prdId);
+    const storedKey = prdId.filter(matchKey);
+    function matchKey(prdId) {
+      const storage = Object.keys(sessionStorage);
+      //   console.log(storage);
+      for (let i = 1; i < storage.length; i++) {
+        const sessionStorageKey = storage[i];
+        // console.log(sessionStorageKey);
+        return sessionStorageKey == prdId;
+      }
+    }
+    const storage = Object.keys(sessionStorage);
+    console.log(storage);
+    if (storedKey.length === 1) {
+      for (let i = 1; i < storage.length; i++) {
+        const storedItem = storage[i];
+        // console.log(storedItem);
+        const myItems = sessionStorage.getItem(storedItem);
         console.log(typeof myItems);
-        if (myItems !== null) {
-          console.log("its working");
-          cartBox.innerHTML += myItems;
-          const removeAdd = document.querySelector(".add-icon");
-          removeAdd.remove();
-          const removeLike = document.querySelector(".like-icon");
-          removeLike.remove();
-          const removeMark = document.querySelector(".mark");
-          removeMark.remove();
-          document.querySelector(".product-sample").className = "cart-box-item";
-          gotoCheck.style.display = "inline-block";
-          continue;
-        }
+        cartBox.innerHTML += myItems;
+        const removeAdd = document.querySelector(".add-icon");
+        removeAdd.remove();
+        const removeLike = document.querySelector(".like-icon");
+        removeLike.remove();
+        const removeMark = document.querySelector(".mark");
+        removeMark.remove();
+        document.querySelector(".product-sample").className = "cart-box-item";
+        gotoCheck.style.display = "inline-block";
+        continue;
       }
     } else {
       cartBox.innerHTML = "Your Cart Is Empty";
